c1e5eb47eeb61dc071aab53f9d7389a490449dee
Dehya Qalbi <dehyasvibe@gmail.com>
2024-05-25 01:01:43 +0700

    enum

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-//1, 'John'
-let user: [number, string] = [1, 'John'];
+// const small = 1;
+// const medium = 2;
+// const large = 3;
 
+//PascalCase
+const enum Size {Small = 1, Medium, Large};
+let mySize: Size = Size.Medium;
+console.log(mySize);
