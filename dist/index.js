c82aad453eb1ef2d4557c06e2c0249339309d99f
Dehya Qalbi <dehyasvibe@gmail.com>
2024-05-25 02:38:09 +0700

    narrowing

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,10 @@
 "use strict";
-;
-let mySize = 2;
-console.log(mySize);
+function kgToLbs(weight) {
+    if (typeof weight === 'number')
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+kgToLbs(10);
+kgToLbs('5kg');
 //# sourceMappingURL=index.js.map
