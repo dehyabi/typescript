0319f7d0fa4a31f2f122d77cd75b9f8295dde7cd
Dehya Qalbi <dehyasvibe@gmail.com>
2024-05-25 02:19:38 +0700

    object

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-function calculateTax(income: number): number, taxYear: number) {
-    let x;
-    if(taxYear || 220_000) {
-        return income * 1.2;
+let employee: {
+    readonly id: number,
+    name: string,
+    retire: (date: Date) => void
+} = { 
+    id: 1, 
+    name: 'John',
+    retire: (date: Date) => {
+        console.log(date);
     }
-    return income * 1.3;
-}
-
-calculateTax(10_000);
+};
